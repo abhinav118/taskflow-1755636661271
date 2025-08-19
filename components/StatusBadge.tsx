aa03f3c3a55e10e9fbdfe46ac4df05e8d46b9563
Abhi <abhinav118@gmail.com>
2025-08-19 16:51:10 -0400

    Add components/StatusBadge.tsx

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,12 @@
+interface StatusBadgeProps {
+  size?: any
+  variant?: any
+}
+
+export default function StatusBadge({ size, variant }: StatusBadgeProps) {
+  return (
+    <div className="px-2 py-1 rounded-full text-xs font-medium">
+      {/* StatusBadge component content */}
+    </div>
+  )
+}

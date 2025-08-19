a68b542d75f10c785e3fa0069c593c25cff27b95
Abhi <abhinav118@gmail.com>
2025-08-19 16:51:10 -0400

    Add components/PriorityIndicator.tsx

---
+++ components/PriorityIndicator.tsx
@@ -0,0 +1,12 @@
+interface PriorityIndicatorProps {
+  shape?: any
+  size?: any
+}
+
+export default function PriorityIndicator({ shape, size }: PriorityIndicatorProps) {
+  return (
+    <div className="w-3 h-3 rounded-full">
+      {/* PriorityIndicator component content */}
+    </div>
+  )
+}
